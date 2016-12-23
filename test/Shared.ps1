835cfafdfce229121dd7debe507ade0c142025b1
Craig Gumbley <craiggumbley@gmail.com>
2016-12-23 14:51:32 +0000

    shared test resource

--- test/Shared.ps1
+++ test/Shared.ps1
@@ -6,6 +6,14 @@ $ModuleManifestPath = "$PSScriptRoot\..\src\$ModuleManifestName"
 if (!$SuppressImportModule) {
     # -Scope Global is needed when running tests from inside of psake, otherwise
     # the module's functions cannot be found in the GitHubReleaseManager\ namespace
-    Import-Module $ModuleManifestPath -Scope Global
+    Import-Module $ModuleManifestPath -Scope Global -Force
 }
 
+$RepositoryName = "TestRepository"
+$Username = "chenak"
+$APIKey = Get-Content -Path ..\.gittoken -Raw
+
+$ReleaseName = "TestRelease-$(Get-Random -Maximum 50)"
+$ReleaseTag = "v0.0.1-$(Get-Random -Maximum 50)"
+
+Set-GithubSessionInformation -Username $Username -APIKey $APIKey | Out-Null
