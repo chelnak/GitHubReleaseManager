c7c97875515dc4eef6e3b710e69632f93ea0e5be
Craig Gumbley <craiggumbley@gmail.com>
2016-12-23 12:10:59 +0000

    Moves two private functions to public region

---
+++ src/Functions/Public/Get-GitHubSessionInformation.ps1
@@ -0,0 +1,35 @@
+function Get-GitHubSessionInformation {
+<#
+    .SYNOPSIS
+    Private function to retrieve a sessionstate variable and check for it's existence
+
+    .DESCRIPTION
+    Private function to retrieve a sessionstate variable and check for it's existence
+
+    .INPUTS
+    None.
+
+    .OUTPUTS
+    System.Management.Automation.PSObject
+
+    .EXAMPLE
+    Get-GitHubSessionInformation
+
+#>
+[CmdletBinding()][OutputType('System.Management.Automation.PSObject')]
+
+    Param()
+
+    Write-Verbose -Message "Retrieving GithubSessionInformation"
+
+    $SessionInfo = $PSCmdlet.GetVariableValue("GitHubSessionInformation")
+
+    if (!$SessionInfo) {
+
+        throw "The GithubSessionInformation does not exist. Run Set-GitHubSessionInformation first!"
+
+    }
+
+    Write-Output $SessionInfo
+
+}
---
+++ src/Functions/Public/Invoke-GitHubRestMethod.ps1
@@ -0,0 +1,136 @@
+function Invoke-GitHubRestMethod {
+<#
+    .SYNOPSIS
+    A module specific wrapper for Invoke-ResetMethod
+
+    .DESCRIPTION
+    A module specific wrapper for Invoke-ResetMethod
+
+    .PARAMETER Method
+    METHOD: GET, POST, PUT, DELETE
+
+    .PARAMETER URI
+    Service URI
+
+    .PARAMETER Body
+    Payload for the request, if applicable
+
+    .PARAMETER InFile
+    The File to upload
+
+    .PARAMETER ContentType
+    The content type of the file to upload
+
+    .PARAMETER Headers
+    Optional Headers to send. This will override the default set provided
+
+    .INPUTS
+    System.String
+
+    .OUTPUTS
+    System.Management.Automation.PSObject
+
+    .EXAMPLE
+    Invoke-GitHubRestMethod -Method POST -URI /api/release/1
+
+#>
+[CmdletBinding(DefaultParameterSetName="Standard")][OutputType('System.Management.Automation.PSObject')]
+
+    Param (
+
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("HEAD","GET","POST","PUT","DELETE")]
+        [String]$Method,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$URI,
+
+        [Parameter(Mandatory=$false, ParameterSetName="WithBody")]
+        [ValidateNotNullOrEmpty()]
+        [String]$Body,
+
+        [Parameter(Mandatory=$false, ParameterSetName="InFile")]
+        [ValidateNotNullOrEmpty()]
+        [String]$InFile,
+
+        [Parameter(Mandatory=$false, ParameterSetName="InFile")]
+        [ValidateNotNullOrEmpty()]
+        [String]$ContentType,
+
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [System.Collections.IDictionary]$Headers
+
+    )
+
+    # --- Build full URI
+    $BaseURI = "https://api.github.com"
+    $FullURI = "$($BaseUri)$($URI)"
+
+    # --- Grab the sessionstate variable & test throw if it is null
+    $SessionInfo = Get-GitHubSessionInformation -Verbose:$VerbosePreference
+
+    # --- If the headers parameter is not passed use the deafult
+    if (!$PSBoundParameters.ContainsKey("Headers")) {
+
+        $Headers = @{
+
+            "Accept" = "application/vnd.github.v3+json"
+            "Authorization" = "Basic $($SessionInfo.Authorization)"
+        }
+
+    }
+
+    try {
+
+
+        switch ($PSCmdlet.ParameterSetName) {
+
+            'WithBody' {
+
+                $Response = Invoke-RestMethod -Method $Method -Headers $Headers -Uri $FullURI -Body $Body -Verbose:$VerbosePrefernce
+                break
+
+            }
+
+            'Infile' {
+
+                $UploadURI = "https://uploads.github.com"
+                $FullURI = "$($UploadURI)$($URI)"
+                $Response = Invoke-WebRequest -Method $Method -Uri $FullURI -Headers $Headers -InFile $InFile -ContentType $ContentType -Verbose:$VerbosePreference
+                break
+
+            }
+
+            default {
+
+                $Response = Invoke-RestMethod -Method $Method -Headers $Headers -Uri $FullURI -Verbose:$VerbosePreference
+                break
+
+            }
+
+        }
+
+    }
+    catch [System.Net.WebException]{
+
+        $PSCallStack = Get-PSCallStack
+        throw "$($PSCallStack[1].Command) $($_)"
+
+    }
+    catch [Exception] {
+
+        throw $_.Exception.Message
+
+    }
+    finally {
+
+        $ServicePoint = [System.Net.ServicePointManager]::FindServicePoint($FullURI)
+        $ServicePoint.CloseConnectionGroup("") | Out-Null
+
+    }
+
+    Write-Output $Response
+
+}
